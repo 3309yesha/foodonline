ba704004fc7edd019b588f71bac06061f7cf3504
yesha <yesha@example.com>
2024-03-21 21:21:33 +0530

    paypal

--- orders/views.py
+++ orders/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from marketplace.models import Cart
 from marketplace.context_processors import get_cart_amounts
-from .models import Order
+from .models import Order,  Payment
 import simplejson as json
 from .forms import OrderForm
 from.utils import generate_order_number
+
+
 
 def place_order(request):
     cart_items = Cart.objects.filter(user=request.user).order_by('created_at')
@@ -55,4 +57,43 @@ def place_order(request):
 
 
 def payments(request):
+        # Check if the request is ajax or not
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest' and request.method == 'POST':
+        # STORE THE PAYMENT DETAILS IN THE PAYMENT MODEL
+        order_number = request.POST.get('order_number')
+        transaction_id = request.POST.get('transaction_id')
+        print(transaction_id)
+        payment_method = request.POST.get('payment_method')
+        status = request.POST.get('status')
+
+        order = Order.objects.get(user=request.user, order_number=order_number)
+        payment = Payment(
+            user = request.user,
+            transaction_id = transaction_id,
+            payment_method = payment_method,
+            amount = order.total,
+            status = status
+        )
+        payment.save()
+
+        # UPDATE THE ORDER MODEL
+        order.payment = payment
+        order.is_ordered = True
+        order.save()
+        return HttpResponse("done")
+
+
+        
+
+        # MOVE THE CART ITEMS TO ORDERED FOOD MODEL
+            
+        # SEND ORDER CONFIRMATION EMAIL TO THE CUSTOMER
+            
+        # SEND ORDER RECEIVED EMAIL TO THE VENDOR
+            
+        # CLEAR THE CART IF THE PAYMENT IS SUCCESS
+        # cart_items.delete() 
+
+        # RETURN BACK TO AJAX WITH THE STATUS SUCCESS OR FAILURE
+        
     return HttpResponse('Payments view')
